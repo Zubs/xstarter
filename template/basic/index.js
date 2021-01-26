912f2977bfe599ebc4de6471786155d1cfd4615c
Zubs <zubairidrisaweda@gmail.com>
2021-01-26 16:14:02 +0100

    Handled 404

--- template/basic/index.js
+++ template/basic/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const terminalLink = require('terminal-link');
+const chalk = require('chalk');
 
 // Import routes
 const routes = require('./routes/web');
@@ -21,6 +22,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routes
 app.use('/', routes);
+
+// Handle 404
+app.use('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '/views', '404.html'));
+});
 
 // Start app
 app.listen(PORT, console.log("Application Started At PORT " + PORT + "\nApplication can be found at " + terminalLink(chalk.yellow.bold('http://localhost:' + PORT), 'http://localhost:' + PORT)));
--- template/ejs/index.js
+++ template/ejs/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const terminalLink = require('terminal-link');
+const chalk = require('chalk');
 
 // Import routes
 const routes = require('./routes/web');
@@ -24,6 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routes
 app.use('/', routes);
+
+// Handle 404
+app.use('*', (req, res) => {
+	res.render('404');
+});
 
 // Start app
 app.listen(PORT, console.log("Application Started At PORT " + PORT + "\nApplication can be found at " + terminalLink(chalk.yellow.bold('http://localhost:' + PORT), 'http://localhost:' + PORT)));
